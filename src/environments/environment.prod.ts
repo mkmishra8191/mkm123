701bb6ade43cdb05487ce54a7491e1db3f00360c
mkmishra8191 <manjeet.eng8191@gail.com>
2020-07-19 02:28:00 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA01VQktZF8bqBnqZnqFIft72l1urKSObU",
+    authDomain: "projects-4cd31.firebaseapp.com",
+    databaseURL: "https://projects-4cd31.firebaseio.com",
+    projectId: "projects-4cd31",
+    storageBucket: "projects-4cd31.appspot.com",
+    messagingSenderId: "465862800054",
+    appId: "1:465862800054:web:4f9622cd6c1ed95c776c17",
+    measurementId: "G-7X1DG075FV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA01VQktZF8bqBnqZnqFIft72l1urKSObU",
+    authDomain: "projects-4cd31.firebaseapp.com",
+    databaseURL: "https://projects-4cd31.firebaseio.com",
+    projectId: "projects-4cd31",
+    storageBucket: "projects-4cd31.appspot.com",
+    messagingSenderId: "465862800054",
+    appId: "1:465862800054:web:4f9622cd6c1ed95c776c17",
+    measurementId: "G-7X1DG075FV"
+  }
 };
 
 /*
